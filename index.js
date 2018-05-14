47f5be212929ee0192390eb83bef2c6afe58e4b6
kellerdevin <36323262+kellerdevin@users.noreply.github.com>
2018-05-13 21:45:45 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import Button from './src/Button';
+import UserAvatar from './src/UserAvatar';
+
+AppRegistry.registerComponent('ProdPageV8', () => App);
